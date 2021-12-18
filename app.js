82fb4ca8aa1415e28c42ab505b7435ffc20a289b
“Seri <ps0512@naver.com>
2021-12-19 01:36:46 +0900

    .

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
-var bakeryRouter = require('./routes/bakery');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/main', mainRouter);
-app.use('/bakery', bakeryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
